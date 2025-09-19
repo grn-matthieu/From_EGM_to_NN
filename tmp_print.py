c55d121f5bbdd5b2ca259fcf622e2062ba1eab22
grn-matthieu <grn.matthieu@yahoo.com>
2025-09-19 17:15:29 +0200

    chore : delete deprecated scripts

--- tmp_print.py
+++
@@ -1,5 +0,0 @@
-import pathlib
-text = pathlib.Path('test/core/test_config_validator.jl').read_text(encoding='utf-8').replace('\r\n','\n')
-block_start = text.index('# ascii params mapping: s -> σ')
-block_end = text.index('# β out of range', block_start)
-print(text[block_start:block_end].encode('unicode_escape').decode())
